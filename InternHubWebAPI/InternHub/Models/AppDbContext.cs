48b7ce5f4ede59bdab22afa7a90aae7d02efe7b6
BeDauPro <tbnduc2k3@gmail.com>
2025-04-02 14:54:14 +0700

    aindatabase

--- InternHubWebAPI/InternHub/Models/AppDbContext.cs
+++ InternHubWebAPI/InternHub/Models/AppDbContext.cs
@@ -6,6 +6,16 @@ namespace InternHub.Models
 {
     public class AppDbContext : IdentityDbContext<ApplicationUser>
     {
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Employer> Employers { get; set; }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Event> Events { get; set; }
+        public DbSet<JobPosting> JobPostings { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<ApplicationHistory> ApplicationHistories { get; set; }
+        public DbSet<StudentReview> StudentReviews { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
         public DbSet<RefreshToken> RefreshTokens { get; set; }
 
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
@@ -13,6 +23,17 @@ namespace InternHub.Models
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Student>().ToTable("Students");
+            modelBuilder.Entity<Employer>().ToTable("Employers");
+            modelBuilder.Entity<Admin>().ToTable("Admins");
+
+            modelBuilder.Entity<Event>().ToTable("Events");
+            modelBuilder.Entity<JobPosting>().ToTable("JobPostings");
+            modelBuilder.Entity<Application>().ToTable("Applications");
+            modelBuilder.Entity<ApplicationHistory>().ToTable("ApplicationHistories");
+            modelBuilder.Entity<StudentReview>().ToTable("StudentReviews");
+            modelBuilder.Entity<Notification>().ToTable("Notifications");
 
             // Thiết lập khóa chính cho IdentityUserLogin
             modelBuilder.Entity<IdentityUserLogin<string>>().HasKey(x => new { x.LoginProvider, x.ProviderKey });
@@ -22,6 +43,7 @@ namespace InternHub.Models
                 .HasOne(rt => rt.User)
                 .WithMany()
                 .HasForeignKey(rt => rt.UserId);
+
         }
     }
 }
---
+++ InternHubWebAPI/InternHub/Models/StudentReview.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace InternHub.Models
+{
+    public class StudentReview
+    {
+        [Key]
+        public int ReviewId { get; set; }
+
+        [Required]
+        [ForeignKey("Student")]
+        public string StudentId { get; set; }
+        public virtual Student Student { get; set; }
+
+        [Required]
+        [ForeignKey("Employer")]
+        public string EmployerId { get; set; }
+        public virtual Employer Employer { get; set; }
+
+        [Required]
+        [ForeignKey("JobPosting")]
+        public int JobPostingId { get; set; }
+        public virtual JobPosting JobPosting { get; set; }
+
+        [Required]
+        public int OverallRating { get; set; }
+
+        public string Comments { get; set; }
+
+        [Required]
+        public string ReviewType { get; set; } // company, job, student
+
+        public DateTime ReviewedAt { get; set; } = DateTime.UtcNow;
+    }
+
+}
+
