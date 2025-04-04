d996c01c33f7668f1017d8f9c43bf68caa069b25
trangiahuy1477 <trgiahuy2906@gmail.com>
2025-04-04 13:45:40 +0700

    Commit 2 file VIewModel  cho danh sách công việc và chi tiết công việc

---
+++ InternHubWebAPI/InternHub/Models/ViewModels/JobPostingViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace InternHub.Models.ViewModels
+{ 
+ 
+    public class JobPostingViewModel 
+    {
+        public int JobPostingId { get; set; }
+        public string JobTitle { get; set; }
+        public string JobCategory { get; set; }
+        public string Location { get; set; }
+        public string WorkType { get; set; }
+        public int Vacancies { get; set; }
+        public DateTime PostedAt { get; set; }
+        public string CompanyName { get; set; }
+        public string CompanyLogo { get; set; }
+    }
+}
