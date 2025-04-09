ab9b1b656d1acb422e8299eeee22405f0c59dce2
BeDauPro <tbnduc2k3@gmail.com>
2025-04-09 20:14:30 +0700

    updatestudentprofile

--- InternHubWebAPI/InternHub/Services/Interfaces/IStudentService.cs
+++ InternHubWebAPI/InternHub/Services/Interfaces/IStudentService.cs
@@ -4,11 +4,12 @@ namespace InternHub.Services.Interfaces
 {
     public interface IStudentService
     {
-        Task<List<StudentDto>> GetAllAsync();
-        Task<StudentDto> GetByIdAsync(int id);
-        Task<StudentDto> CreateAsync(CreateStudentDto dto, IWebHostEnvironment env);
-        Task<StudentDto> UpdateAsync(int id, UpdateStudentDto dto, IWebHostEnvironment env, bool isEmployee);
-        Task<bool> DeleteAsync(int id);
+        Task<List<StudentDto>> GetAllAsync(bool isStudent = false);
+        Task<StudentDto?> GetByUserIdAsync(string userID);
+        Task<StudentDto?> GetByIdAsync(int id);
+        Task<StudentDto> CreateAsync(CreateStudentDto dto, string userId, IWebHostEnvironment env);
+        Task<StudentDto> UpdateAsync(int id, UpdateStudentDto dto, IWebHostEnvironment env, bool isEmployer, string userId);
+        Task<bool> DeleteAsync(int id,  string userId);
         List<string> GetStatuses();
     }
 }
